0ea2ce7d6433b9271642398dff58f57f768dd434
josh1542 <131529460+josh1542@users.noreply.github.com>
2026-08-18 20:55:21 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "AZ-104 App Service Lab - Deployment Successful");
+var message = builder.Configuration["AZ104_MESSAGE"]
+              ?? "AZ104_MESSAGE is not configured";
+
+app.MapGet("/", () => $"AZ-104 App Service Lab - {message}");
 
 app.Run();
